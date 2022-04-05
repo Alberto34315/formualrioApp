fc7fc9bec2fc8cb5761e4beca44032290c602d88
Alberto Ruiz <ruizserranoalberto5@gmail.com>
2022-04-05 18:50:02 +0200

    first commit

--- src/app/auth/pages/registo/registo.component.ts
+++ src/app/auth/pages/registo/registo.component.ts
@@ -6,6 +6,7 @@ import {
   Validators,
   FormControl,
 } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registo',
@@ -13,28 +14,42 @@ import {
   styles: [],
 })
 export class RegistoComponent implements OnInit {
-  
+
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: [
-      '',
-      [Validators.required, Validators.pattern(this.vs.nombreApellidosPattern)],
-    ],
-    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+    nombre: ['', [Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.vs.noPuedeSerStrider]],
-    password: ['', [Validators.required,Validators.minLength(6)]], 
+    password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
-  },{
-    Validators:[this.vs.camposIguales('password','password2')]
+  }, {
+    validators: [this.vs.camposIguales('password', 'password2')]
   });
 
-  constructor(private fb: FormBuilder,private vs:ValidatorService) {}
+  get emailErrorMsg() {
+    const errors = this.miFormulario.get("email")?.errors;
+    let msg: string = ""
+    if (errors?.['required']) {
+      msg = 'Email es obligatorio'
+    } else if (errors?.['pattern']) {
+      msg = 'El valor no tiene formato de correo'
+    } else if (errors?.['emailTomado']) {
+      msg = 'El correo electrónico ya fue tomado'
+    }
+    return msg;
+  }
+
+  constructor(private fb: FormBuilder,
+    private vs: ValidatorService,
+    private emailValidator: EmailValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Paco Candela',
       email: 'test1@test.com',
-      username:"paquito1"
+      username: "paquito1",
+      password: "123456",
+      password2: "123456"
     });
   }
 
@@ -45,7 +60,25 @@ export class RegistoComponent implements OnInit {
     );
   }
 
+
+
+  // emailRequired() {
+  //   return this.miFormulario.get('email')?.errors?.['required']
+  //     && this.miFormulario.get('email')?.touched;
+  // }
+  // emailFormato() {
+  //   return this.miFormulario.get('email')?.errors?.['pattern']
+  //     && this.miFormulario.get('email')?.touched;
+  // }
+  // emailTomado() {
+  //   return this.miFormulario.get('email')?.errors?.['emailTomado']
+  //     && this.miFormulario.get('email')?.touched;
+  // }
+
+
   submitFormulario() {
     this.miFormulario.markAllAsTouched();
+    console.log(this.miFormulario.value);
   }
+
 }
--- src/app/shared/validator/validator.service.ts
+++ src/app/shared/validator/validator.service.ts
@@ -5,10 +5,10 @@ import { AbstractControl, FormControl, ValidationErrors } from '@angular/forms';
   providedIn: 'root',
 })
 export class ValidatorService {
-  public nombreApellidosPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   public emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
-  constructor() {}
+  constructor() { }
 
   noPuedeSerStrider(control: FormControl): ValidationErrors | null {
     const valor: string = control.value?.trim().toLowerCase();
@@ -21,18 +21,23 @@ export class ValidatorService {
   }
 
   camposIguales(campo1: string, campo2: string) {
+
     return (formGroup: AbstractControl): ValidationErrors | null => {
-      console.log(formGroup);
-      
+
       const pass1 = formGroup.get(campo1)?.value;
       const pass2 = formGroup.get(campo2)?.value;
+
       if (pass1 !== pass2) {
         formGroup.get(campo2)?.setErrors({ noIguales: true });
-        return { noIguales: true };
+        return { noIguales: true }
       }
+
+
+
       formGroup.get(campo2)?.setErrors(null);
 
-      return null;
-    };
+      return null
+    }
+
   }
 }
